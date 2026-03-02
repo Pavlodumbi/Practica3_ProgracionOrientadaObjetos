64693f1064f1468a456f5eb66b2081a60e754f6f
Pablo Trujillo <josepablots25@gmail.com>
2026-03-02 12:35:04 -0800

    Se creo la clase DadoZombie donde se pone logica para que decida de acuerdo al color, que posiblesValores tomar

---
+++ DadoZombie.java
@@ -0,0 +1,67 @@
+
+/**
+ * Write a description of class DadoZombie here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class DadoZombie
+{
+    private String valor;
+    private String color;
+    private Dado dado;
+    private String[] posiblesValores;
+    /**
+     * Constructor for objects of class DadoZombie
+     */
+    public DadoZombie(String color)
+    {
+        dado = new Dado();
+        posiblesValores = new String[6];
+        decidirPosiblesValores();
+    }
+    
+    private void decidirPosiblesValores(){
+         if(color == "green"){
+            for(int i = 0; i < 6; i++){
+                 if(i>=0 && i<=2)
+                     posiblesValores[i] = "brain";
+                 else if(i>=3 && i <=4)
+                     posiblesValores[i] = "runner";
+                 else
+                    posiblesValores[i] = "shotgun";
+            }
+        }
+        else if(color == "yellow"){
+            for(int i = 0; i < 6; i++){
+                if(i>=0 && i<=1)
+                    posiblesValores[i] = "brain";
+                else if(i >=2 && i <=3)
+                posiblesValores[i] = "runner";
+                else
+                posiblesValores[i] = "shotgun";
+            }
+        }
+        else{
+            for(int i = 0; i < 6; i++){
+                if(i==0)
+                    posiblesValores[i] = "brain";
+                else if(i >=1 && i <=2)
+                posiblesValores[i] = "runner";
+                else
+                posiblesValores[i] = "shotgun";
+            }
+        }
+    }
+    
+    public void lanzar(){
+         dado.lanzar();
+    }
+
+    public String getValor(){
+        int temp = dado.getValor();
+        valor = posiblesValores[temp - 1];
+        return valor;
+    }
+
+}
---
+++ JugadorZombie.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class JugadorZombie here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class JugadorZombie
+{
+    private String nombre;
+    private int cerebrosTotales;
+    private int cerebrosTemporales;
+    private int corredores;
+    private int escopetas;
+    /**
+     * Constructor for objects of class JugadorZombie
+     */
+    public JugadorZombie()
+    {
+
+    }
+
+}
