58c8608dca7133c42cd51a71375fde39150a405a
Pablo Trujillo <josepablots25@gmail.com>
2026-03-02 20:55:23 -0800

    Se agregaron unos metodos al juego para lanzar, procesar, y ver los resultados de la jugada

--- JuegoZombieDice.java
+++ JuegoZombieDice.java
@@ -20,20 +20,48 @@ public class JuegoZombieDice
     {
         jugadorEnTurno = 0;
         this.cantidadJugadores = cantidadJugadores;
+
         jugadores = new ArrayList(cantidadJugadores);
+        bolsa = new BolsaDados();
         dadosEnJuego = new ArrayList(3);
     }
-    
+
     public void agregarJugadores(String nombre){
         if(jugadores.size() < cantidadJugadores){
             jugadores.add(new JugadorZombie(nombre));
         }
     }
-    
+
     public void AgarrarDadosBolsa(){
         JugadorZombie jugador = jugadores.get(jugadorEnTurno);
         int corredoresActu = jugador.getCorredores();
-        dadosEnJuego = bolsa.agarrarDados(3-corredoresActu);
+        dadosEnJuego = bolsa.agarrarDados(3);
+    }
+
+    public void lanzarDados(){
+        for(int i = 0; i < 3; i++){
+            DadoZombie dado = dadosEnJuego.get(i);
+            dado.lanzar();
+        }
+    }
+
+    public void procesarDados(){
+        for(int i = 0; i < 3; i++){
+            JugadorZombie jugador = jugadores.get(jugadorEnTurno);
+            DadoZombie dado = dadosEnJuego.get(i);
+
+            if(dado.getValor() == "brain")
+                jugador.sumarCerebrosTemporales(1);
+            else if(dado.getValor() == "runner")
+                jugador.sumarCorredores(1);
+            else if(dado.getValor() == "shotgun")
+                jugador.sumarEscopetas(1);
+        }
+    }
+    
+    public JugadorZombie getResultadosJugada(){
+        JugadorZombie jugador = jugadores.get(jugadorEnTurno);
+        return jugador;
     }
 
 }
