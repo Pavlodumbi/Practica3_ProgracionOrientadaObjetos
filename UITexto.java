9839e1ba4d70e65d98465cdc75d8bd2f2086b5ce
Pablo Trujillo <josepablots25@gmail.com>
2026-03-03 19:26:39 -0800

    Se arreglaron unas cosas del scan para que se pudiera visualizar correctamente

--- UITexto.java
+++ UITexto.java
@@ -10,39 +10,47 @@ public class UITexto
 
     public void main(){
         int cantidadJugadores = ingresarCantidadJugadores();
-
         JuegoZombieDice juego = new JuegoZombieDice(cantidadJugadores);
         for(int i = 0; i < cantidadJugadores; i++){
-            Scanner scanear = new Scanner(System.in);
-            System.out.println("Ingresa el nombre del jugador " + (i+1) + ":");
-            juego.agregarJugadores(scanear.nextLine());
+            String nombre = ingresarNombre(i+1);
+            juego.agregarJugadores(nombre);
         }
         
-        System.out.println("Juego ZombieDice comienza!!");
+        
+        System.out.println("Juego Comienza");
+        Scanner scan = new Scanner(System.in);
         while(!juego.hayGanador()){
+            String colores = "";
             JugadorZombie turno = juego.getJugadorActual();
-            System.out.println("Turno del jugador" + turno.getNombre() + ":");
+            System.out.println("Turno del jugador " + turno.getNombre() + ":");
             System.out.println("Cerebros Actuales:" + turno.getCerebros());
             
-            System.out.println("Pulsa Enter para agarrar  3 Dados de la bolsa");
-            Scanner scan = new Scanner(System.in);
-            scan.nextLine();
+            System.out.println("Pulsa Enter para agarrar 3 Dados de la bolsa");
+            String trash = scan.nextLine();
             
             juego.AgarrarDadosBolsa();
             ArrayList<DadoZombie> dados = juego.getDadosAgarrados();
             
             for(int i = 0; i < 3; i++){
                 DadoZombie dado = dados.get(i);
+                colores += dado.getColor() + " ";
             }
+            System.out.println("Han salido 3 dados, con los colores: " + colores);
         }
 
     }
     public int ingresarCantidadJugadores(){
         int cantidad = 0;
         Scanner scan = new Scanner(System.in);
-        System.out.println("Ingresa cantidad Jugadores: ");
+        
+        System.out.print("Cuantos Jugadores: ");
         cantidad = scan.nextInt();
         return cantidad;
     }
-
+    
+    public String ingresarNombre(int idxJugador){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ingresa el nombre del jugador " + idxJugador + ":");
+        return scan.nextLine();
+    }
 }
